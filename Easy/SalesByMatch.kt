d0483623401497738bdc4c84727d0fe248b5f46d
CHRISTENTO SIREGAR <106148417+Tento03@users.noreply.github.com>
2025-11-02 18:12:27 +0700

    sales by match

---
+++ Easy/SalesByMatch.kt
@@ -0,0 +1,52 @@
+import java.io.*
+import java.math.*
+import java.security.*
+import java.text.*
+import java.util.*
+import java.util.concurrent.*
+import java.util.function.*
+import java.util.regex.*
+import java.util.stream.*
+import kotlin.collections.*
+import kotlin.comparisons.*
+import kotlin.io.*
+import kotlin.jvm.*
+import kotlin.jvm.functions.*
+import kotlin.jvm.internal.*
+import kotlin.ranges.*
+import kotlin.sequences.*
+import kotlin.text.*
+
+/*
+ * Complete the 'sockMerchant' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts following parameters:
+ *  1. INTEGER n
+ *  2. INTEGER_ARRAY ar
+ */
+
+fun sockMerchant(n: Int, ar: Array<Int>): Int {
+    // Write your code here
+    var colorMapping=mutableMapOf<Int,Int>()
+    var pairs=0
+    
+    for (color in ar){
+        colorMapping[color]=colorMapping.getOrDefault(color, 0) + 1
+    }
+    
+    for (pair in colorMapping.values){
+        pairs+=pair/2
+    }   
+    return pairs
+}
+
+fun main(args: Array<String>) {
+    val n = readLine()!!.trim().toInt()
+
+    val ar = readLine()!!.trimEnd().split(" ").map{ it.toInt() }.toTypedArray()
+
+    val result = sockMerchant(n, ar)
+
+    println(result)
+}
